e7cf6b2a4fb04ca2fb08e91158a2c13ad5e2981e
Sam <mrsamgroves@outlook.com>
2024-04-06 19:47:46 +0100

    Put the GUI into a Class. Will move over to batchConvert.py

--- testTkinter.py
+++ testTkinter.py
@@ -2,60 +2,66 @@ from tkinter import ttk, messagebox
 import tkinter as tk
 import sys
 
-conversionDriver = ''
-inputDriver = ''
+
 #https://docs.python.org/3.10/library/tkinter.messagebox.html
-def submitSelection():
-    #access outside variable
-    global conversionDriver
-    global inputDriver
-    #use .get() to get the selected value from combobox
+class conversionApp:
 
-    messageBoxInput = messagebox.askyesnocancel(title = 'Confirmation', message='Do you want to submit')
-    if messageBoxInput == True:
-        conversionDriver = comboConvert.get()
-        inputDriver = comboInput.get()
-        mainWindow.destroy()
-        messagebox.showinfo(title = 'Conversion Tool', message='Conversion going ahead!')
-    elif messageBoxInput == False:
-        mainWindow.destroy()
-        messagebox.showinfo(title= 'Conversion Tool', message='Conversion tool ended.')
+    def __init__(self):
+        self.driverOptions = ["DXF","CSV", "OpenFileGDB", "ESRIJSON", "ESRI Shapefile", "FlatGeobuf", 
+    "GeoJSON", "GeoJSONSeq", "GPKG","GML", "OGR_GMT","GPX","Idrisi","MapInfo File",
+    "DGN","PCIDSK","OGR_PDS","S57","SQLite","TopoJSON"]
+        self.conversionDriver = ''
+        self.inputDriver = ''
 
-        sys.exit()
+        self.mainWindow = tk.Tk()
+        self.setupUI()
 
-driverOptions = ["DXF","CSV", "OpenFileGDB", "ESRIJSON", "ESRI Shapefile", "FlatGeobuf", 
- "GeoJSON", "GeoJSONSeq", "GPKG","GML", "OGR_GMT","GPX","Idrisi","MapInfo File",
- "DGN","PCIDSK","OGR_PDS","S57","SQLite","TopoJSON"]
-#Main window
-mainWindow = tk.Tk()
-mainWindow.config(width=400, height=300)
-mainWindow.title('Combobox')
 
-#Convert to section
-comboConvertLabel = ttk.Label(mainWindow, text="Convert to:")
-comboConvertLabel.place(x=200, y=25)
-comboConvert = ttk.Combobox(
-    state='readonly',
-    values=driverOptions
-)
-comboConvert.place(x=200,y=50)
 
-comboInputLabel = ttk.Label(mainWindow, text = "Convert from:")
-comboInputLabel.place(x=50, y=25)
-comboInput = ttk.Combobox(
-    state="readonly",
-    values= driverOptions
-)
-comboInput.place(x=50, y=50)
 
-submitLabel = ttk.Label(mainWindow, text= "Once options are selected, click Submit.")
-submitLabel.place(x=90, y=175)
-submitButton = ttk.Button(mainWindow, text="Submit", command=submitSelection)
-submitButton.place(x=150, y=200)
-""" button = ttk.Button(text = "Display selection", command=displaySelection)
-button.place(x=0, y=0)
- """
-mainWindow.mainloop()
+    def setupUI(self):
+        self.mainWindow.config(width=400, height=300)
+        self.mainWindow.title('GIS File Conversion')
 
-print(conversionDriver)
-print(f"input {inputDriver}")
+        #Convert to section
+        comboConvertLabel = ttk.Label(self.mainWindow, text="Convert to:")
+        comboConvertLabel.place(x=200, y=25)
+        self.comboConvert = ttk.Combobox(
+            state='readonly',
+            values=self.driverOptions
+        )
+        self.comboConvert.place(x=200,y=50)
+
+        comboInputLabel = ttk.Label(self.mainWindow, text = "Convert from:")
+        comboInputLabel.place(x=50, y=25)
+        self.comboInput = ttk.Combobox(
+            state="readonly",
+            values= self.driverOptions
+        )
+        self.comboInput.place(x=50, y=50)
+
+        submitLabel = ttk.Label(self.mainWindow, text= "Once options are selected, click Submit.")
+        submitLabel.place(x=90, y=175)
+        self.submitButton = ttk.Button(self.mainWindow, text="Submit", command=self.submitSelection)
+        self.submitButton.place(x=150, y=200)        
+
+        
+    def submitSelection(self):
+    
+        #use .get() to get the selected value from combobox
+        messageBoxInput = messagebox.askyesnocancel(title = 'Confirmation', message='Do you want to submit')
+        if messageBoxInput == True:
+            self.conversionDriver = self.comboConvert.get()
+            self.inputDriver = self.comboInput.get()
+            self.mainWindow.destroy()
+            messagebox.showinfo(title = 'Conversion Tool', message='Conversion going ahead!')
+        elif messageBoxInput == False:
+            self.mainWindow.destroy()
+            messagebox.showinfo(title= 'Conversion Tool', message='Conversion tool ended.')
+            sys.exit()
+
+    def run(self):
+        self.mainWindow.mainloop()
+ 
+app = conversionApp()
+app.run()
