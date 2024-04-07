d33e503a1ddb09f8b3f179c2c843e256791eeb95
Sam <mrsamgroves@outlook.com>
2024-04-07 13:41:08 +0100

    Dynamic input and export formats added fully.
    - Inputs and Exports now convert based on input and convert to fields.
    - Removed holding variables from __init__ as these were unnecessary.

--- batchConvert.py
+++ batchConvert.py
@@ -6,16 +6,11 @@
 import geopandas as gpd
 import os
 import sys
-
-
-# These are for folder dialogue prompt
+# For GUI
 # https://stackoverflow.com/questions/3964681/find-all-files-in-a-directory-with-extension-txt-in-python
 from tkinter import ttk, messagebox
 from tkinter.filedialog import askdirectory
 import tkinter as tk
-
-
-
 
 # ---- GUI Class definition ----
 class convertGui:
@@ -29,17 +24,13 @@ class convertGui:
             'PCIDSK': '.pix',  'S57': '.000', 'SQLite': '.sqlite', 'TopoJSON': '.topojson'
             }
         self.driverOptions = list(self.driverOptionsDict.keys())
-        #output holding variables to be called in batchConvert
-        self.conversionDriver = ''
-        self.inputDriver = ''
-        self.inputPath = ''
-        self.outputPath = ''
+ 
         #gui main window holding variables
         self.mainWindow = tk.Tk() # usually called root in examples. This is the main window.
-        self.setupUI()
+        self.conversionUI()
 
-        #main window UI of tkinter
-    def setupUI(self):
+        #main window UI of tkinter for conversion tool
+    def conversionUI(self):
         self.mainWindow.config(width=600, height=400)
         self.mainWindow.title('GIS File Conversion')
 
@@ -51,6 +42,7 @@ class convertGui:
             values=self.driverOptions
         )
         self.comboConvert.place(x=350,y=50)
+    
         #Input conversion file type
         comboInputLabel = ttk.Label(self.mainWindow, text = "Convert from:")
         comboInputLabel.place(x=100, y=25)
@@ -84,7 +76,6 @@ class convertGui:
         if self.inputPath:
             self.inputPathLabel.config(text=self.inputPath)
 
-
     #for select output directory button.
     def selectOutputDirectory(self):
         self.outputPath = askdirectory(title="Output directory for converted files")
@@ -93,55 +84,62 @@ class convertGui:
             self.outputPathLabel.config(text=self.outputPath)
 
     def submitSelection(self):
-    
         #use .get() to get the selected value from combobox
         messageBoxInput = messagebox.askyesnocancel(title = 'Confirmation', message='Do you want to submit?')
         if messageBoxInput == True:
+            #Returns conversion Driver and then returns the extension from dict based on that value
             self.conversionDriver = self.comboConvert.get()
+            self.conversionDriverExt = self.driverOptionsDict.get(self.conversionDriver)
+            #Returns input Driver and then returns extension
             self.inputDriver = self.comboInput.get()
+            self.inputDriverExt = self.driverOptionsDict.get(self.inputDriver)
             self.mainWindow.destroy()
             messagebox.showinfo(title = 'Conversion Tool', message='Conversion going ahead!')
+        # If select No -> False -> closes everything and stops script.
         elif messageBoxInput == False:
             self.mainWindow.destroy()
             messagebox.showinfo(title= 'Conversion Tool', message='Conversion tool ended.')
             sys.exit()
+        # If select Cancel -> None -> lets user continue and change settings.
 
     def run(self):
         self.mainWindow.mainloop()
- 
+# Run the gui to input.
 convertFrontEnd = convertGui()
 convertFrontEnd.run()
 
-def batchConvert(guiInput):
-    #reads gis input file
+# Once submit clicked, run the batch convert.
 
+def batchConvert(guiInput):
     #Handles differently converting to geopackage.
     if convertFrontEnd.conversionDriver == 'GPKG':
                  # file name and path creation
         outputFileName = 'Converted.gpkg'
         outputFullPath = os.path.join(guiInput.outputPath, outputFileName)
+
         for file in os.listdir(guiInput.inputPath):
-            if file.endswith(".shp"):
+            #Only files with specific extensions selected from GUI are converted. Can have mixed folder.
+            if file.endswith(guiInput.inputDriverExt):
                 fullPath = os.path.join(guiInput.inputPath, file)
                 gdf = gpd.read_file(fullPath)
                 tableName = os.path.splitext(os.path.basename(fullPath))[0] #removes directory path for just layer name
-                # Creates geopackage, then will append additional layers.
+                # Creates geopackage, then will append additional layers using tableName
                 gdf.to_file(outputFullPath, layer=tableName, driver = 'GPKG')
                 print(f"Converted {file} to GeoJSON and saved as {outputFileName}")
-    # Select files by just .shp
     # https://stackoverflow.com/questions/3964681/find-all-files-in-a-directory-with-extension-txt-in-python
     # shape file conversion: https://stackoverflow.com/questions/43119040/shapefile-into-geojson-conversion-python-3
     else:
         for file in os.listdir(guiInput.inputPath):
-            if file.endswith(".shp"):
+            #Only files with specific extensions selected from GUI are converted. Can have mixed folder.
+            if file.endswith(guiInput.inputDriverExt):
                 fullPath = os.path.join(guiInput.inputPath, file)
                # file name and path creation
-                outputFileName = os.path.splitext(file)[0] + "." + str(guiInput.conversionDriver)
+                outputFileName = os.path.splitext(file)[0] + guiInput.conversionDriverExt
                 outputFullPath = os.path.join(guiInput.outputPath, outputFileName)
                 gdf = gpd.read_file(fullPath)
                 gdf.to_file(outputFullPath, driver=str(guiInput.conversionDriver))
 
-                print(f"Converted {file} to GeoJSON and saved as {outputFileName}")
+                print(f"Converted {file} to {guiInput.conversionDriver} and saved as {outputFileName}")
 
 
 batchConvert(convertFrontEnd)
