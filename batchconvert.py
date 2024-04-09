c2154e3f0fb21b7995ab07c43051974ef5c1d0e5
hularuns <159131400+hularuns@users.noreply.github.com>
2024-04-09 21:50:04 +0100

    V2.0 Geopackages and other DBs can now be fully converted from.
    Full integration of geopackage conversion to other formats.
    
    -All tables within geopackages can now be fully converted to other formats

--- batchconvert.py
+++ batchconvert.py
@@ -2,7 +2,7 @@ import geopandas as gpd
 import os
 import fiona 
 
-def batch_convert(input_path, output_path, input_driver, input_driver_ext, conversion_driver, conversionDriverExt, conversion_crs):
+def batch_convert(input_path, output_path, input_driver, input_driver_ext, conversion_driver, conversion_driver_ext, conversion_crs):
     """Batch Conversion Tool
     Performs batch conversion of GIS files using specified formats and projection.
     
@@ -20,51 +20,79 @@ def batch_convert(input_path, output_path, input_driver, input_driver_ext, conve
     # Handles differently converting to geopackage.
 
     try:
-        for gis_file in os.listdir(input_path):
-            if gis_file.endswith(input_driver_ext):
-                full_path = os.path.join(input_path, gis_file)
-                try:
-                    gdf = gpd.read_file(full_path)
-                except Exception as error:
-                    print(f"Error reading {gis_file}: {error}")
-                    continue  # Skip files that cannot be read
-
-                # converts if conversionCrs is true, otherwise won't convert.
-                if conversion_crs:
-                    gdf = gdf.to_crs(conversion_crs)
+        # Handles differently for multilayer input files.
+        if input_driver == 'GPKG':
+            for gis_file in os.listdir(input_path):
+                if gis_file.endswith(input_driver_ext):
+                    full_path = os.path.join(input_path, gis_file)
                     
-                
+                    for name in fiona.listlayers(full_path):
+                        gdf = gpd.read_file(full_path, layer = name)
+                        print(name)
 
-                # this section converts file type.
-                if conversion_driver == 'GPKG':
-                    output_file_name = 'Converted.gpkg' 
-                    output_full_path = os.path.join(output_path, output_file_name)
-                    table_name = os.path.splitext(os.path.basename(gis_file))[0] #obtains file name of each file adding as layer
-                    # Create or append to geopackage, will change mode depending on if it exists or not.
-                    gdf.to_file(output_full_path, layer=table_name, driver = 'GPKG')
-                    print(f"Converted {gis_file} to {conversion_driver} and saved as a table within {output_file_name} with CRS of {conversion_crs}")           
-                else:
-                    # file name and path creation
-                    output_file_name = os.path.splitext(gis_file)[0] + conversionDriverExt
-                    output_full_path = os.path.join(output_path, output_file_name)
-                    # Creates and saves converted file
-                    # Converts to CSV
-                    if conversion_driver == 'CSV':
-                        gdf.to_csv(output_full_path, index = False, mode='w')
-                    # if DXF just use geometries as it only accepts certain specific support fields.
-                    elif conversion_driver == "DXF":
-                        # extracts only geometry into the gdf for the DXF
-                        gdf = gdf[["geometry"]]
-                        gdf.to_file(output_full_path, driver=str(conversion_driver))
-                    # If any other type:
+                    # converts if conversionCrs is true, otherwise won't convert.
+                    if conversion_crs:
+                        gdf = gdf.to_crs(conversion_crs)
+                    # this section converts file type.
+
+                        # file name and path creation
+                        output_file_name = os.path.splitext(gis_file)[0] + conversion_driver_ext
+                        output_full_path = os.path.join(output_path, output_file_name)
+                        # Creates and saves converted file
+                        # Converts to CSV
+                        if conversion_driver == 'CSV':
+                            gdf.to_csv(output_full_path, layer = name, index = False, mode='w')
+                        # if DXF just use geometries as it only accepts certain specific support fields.
+                        elif conversion_driver == "DXF":
+                            # extracts only geometry into the gdf for the DXF
+                            gdf = gdf[["geometry"]]
+                            gdf.to_file(output_full_path, layer = name, driver=str(conversion_driver))
+                        # If any other type:
+                        else:
+                            gdf.to_file(output_full_path, layer=name, driver=str(conversion_driver))
+
+                        print(f"Converted {gis_file} to {conversion_driver} and saved as {output_file_name}")
+        
+        else:
+            for gis_file in os.listdir(input_path):
+                if gis_file.endswith(input_driver_ext):
+                    full_path = os.path.join(input_path, gis_file)
+                    try:
+                        gdf = gpd.read_file(full_path)
+                    except Exception as error:
+                        print(f"Error reading {gis_file}: {error}")
+                        continue  # Skip files that cannot be read
+                    # converts if conversionCrs is true, otherwise won't convert.
+                    if conversion_crs:
+                        gdf = gdf.to_crs(conversion_crs)
+                            
+                    # this section converts file type.
+                    if conversion_driver == 'GPKG':
+                        output_file_name = 'Converted.gpkg' 
+                        output_full_path = os.path.join(output_path, output_file_name)
+                        table_name = os.path.splitext(os.path.basename(gis_file))[0] #obtains file name of each file adding as layer
+                        # Create or append to geopackage, will change mode depending on if it exists or not.
+                        gdf.to_file(output_full_path, layer=table_name, driver = 'GPKG')
+                        print(f"Converted {gis_file} to {conversion_driver} and saved as a table within {output_file_name} with CRS of {conversion_crs}")           
                     else:
-                        gdf.to_file(output_full_path, driver=str(conversion_driver))
+                        # file name and path creation
+                        output_file_name = os.path.splitext(gis_file)[0] + conversion_driver_ext
+                        output_full_path = os.path.join(output_path, output_file_name)
+                        # Creates and saves converted file
+                        # Converts to CSV
+                        if conversion_driver == 'CSV':
+                            gdf.to_csv(output_full_path, index = False, mode='w')
+                        # if DXF just use geometries as it only accepts certain specific support fields.
+                        elif conversion_driver == "DXF":
+                            # extracts only geometry into the gdf for the DXF
+                            gdf = gdf[["geometry"]]
+                            gdf.to_file(output_full_path, driver=str(conversion_driver))
+                        # If any other type:
+                        else:
+                            gdf.to_file(output_full_path, driver=str(conversion_driver))
 
-                    print(f"Converted {gis_file} to {conversion_driver} and saved as {output_file_name}")
-    # should print errors to console
+                        print(f"Converted {gis_file} to {conversion_driver} and saved as {output_file_name}")
+        # should print errors to console
     except Exception as error:
         print(error)                    
-
-
-# batchConvert(convertFrontEnd)
 
